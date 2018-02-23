7c892d1dd351aba3a687f4b2916567d8ed19081d
Monster <textbrowser@gmail.com>
2018-02-23 18:58:25 -0500

    Corrected.

--- spot-on-lite-daemon.sh
+++ spot-on-lite-daemon.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-su - c "cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf" spot-on-lite-daemon
+su -c "cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf" spot-on-lite-daemon
 exit $?
