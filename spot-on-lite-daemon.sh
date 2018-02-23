f3ccb56241df3d53adb32750c89cecd578f2a8b7
Monster <textbrowser@gmail.com>
2018-02-23 18:56:31 -0500

    Launch as spot-on-lite-daemon.

--- spot-on-lite-daemon.sh
+++ spot-on-lite-daemon.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file \
-	/usr/local/spot-on-lite/spot-on-lite-daemon.conf
+su - c "cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf" spot-on-lite-daemon
 exit $?
