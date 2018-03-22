743226717bceddf2b3c6c94bb6dc58efb4a28ccb
Monster <textbrowser@gmail.com>
2018-03-22 09:27:26 -0400

    Source some initialization functions.

--- spot-on-lite-daemon.sh
+++ spot-on-lite-daemon.sh
@@ -9,13 +9,13 @@
 # Description:       The cute and wonderful Spot-On-Lite-Daemon.
 ### END INIT INFO
 
+. /lib/lsb/init-functions
+
 case "$1" in
   start)
-      echo -n "Starting Spot-On-Lite-Daemon... "
       su -c "cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf" spot-on-lite-daemon
       ;;
   stop)
-      echo -n "Stopping all Spot-On-Lite-Daemon processes owned by spot-on-lite-daemon... "
       killall -u spot-on-lite-daemon Spot-On-Lite-Daemon
       ;;
   *)
