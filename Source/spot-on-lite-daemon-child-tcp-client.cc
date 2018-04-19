23ae3e4ea2df7696c50a7f957ffb45fe63df4886
Monster <textbrowser@gmail.com>
2018-04-18 22:38:13 -0400

    Removed timeout.

--- Source/spot-on-lite-daemon-child-tcp-client.cc
+++ Source/spot-on-lite-daemon-child-tcp-client.cc
@@ -915,7 +915,7 @@ void spot_on_lite_daemon_child_tcp_client::process_data(void)
 
   while(!m_abort.fetchAndAddOrdered(0))
     {
-      m_wait_condition.wait(lock.mutex(), 1500);
+      m_wait_condition.wait(lock.mutex());
       lock.unlock();
 
       if(m_abort.fetchAndAddOrdered(0))
