8be42089ab7fb7c8f63d0344dda66936803263b8
Monster <textbrowser@gmail.com>
2018-05-19 20:31:55 -0400

    License information.

--- Source/spot-on-lite-daemon-sha.lisp
+++ Source/spot-on-lite-daemon-sha.lisp
@@ -1,3 +1,28 @@
+;; Copyright (c) 2011 - 10^10^10, Alexis Megas.
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;; 1. Redistributions of source code must retain the above copyright
+;;    notice, this list of conditions and the following disclaimer.
+;; 2. Redistributions in binary form must reproduce the above copyright
+;;    notice, this list of conditions and the following disclaimer in the
+;;    documentation and/or other materials provided with the distribution.
+;; 3. The name of the author may not be used to endorse or promote products
+;;    derived from Spot-On without specific prior written permission.
+;;
+;; SPOT-ON-LITE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+;; IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+;; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+;; IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+;; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+;; NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+;; SPOT-ON-LITE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 (defun Ch (x y z)
   (logxor (logand x y) (logand (lognot x) z)))
 
@@ -32,3 +57,5 @@
 (setf (aref s_sha_512_h 5) (parse-integer "9b05688c2b3e6c1f" :radix 16))
 (setf (aref s_sha_512_h 6) (parse-integer "1f83d9abfb41bd6b" :radix 16))
 (setf (aref s_sha_512_h 7) (parse-integer "5be0cd19137e2179" :radix 16))
+
+(setf s_sha_512_k (make-array 80))
