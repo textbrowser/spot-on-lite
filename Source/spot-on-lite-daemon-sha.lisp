c9d01cb6339f6ae5706a522e861570fbcf1c7c62
Alexis Megas <textbrowser@gmail.com>
2018-06-07 06:19:59 -0400

    Initialize global variables.

--- Source/spot-on-lite-daemon-sha.lisp
+++ Source/spot-on-lite-daemon-sha.lisp
@@ -56,100 +56,100 @@
 )
 
 (defvar s_sha_512_h
-  (setf s_sha_512_h (make-array 8
-				:element-type '(unsigned-byte 64)
-				:initial-contents '(#x6a09e667f3bcc908
-						    #xbb67ae8584caa73b
-						    #x3c6ef372fe94f82b
-						    #xa54ff53a5f1d36f1
-						    #x510e527fade682d1
-						    #x9b05688c2b3e6c1f
-						    #x1f83d9abfb41bd6b
-						    #x5be0cd19137e2179))))
+  (make-array 8
+	      :element-type '(unsigned-byte 64)
+	      :initial-contents '(#x6a09e667f3bcc908
+				  #xbb67ae8584caa73b
+				  #x3c6ef372fe94f82b
+				  #xa54ff53a5f1d36f1
+				  #x510e527fade682d1
+				  #x9b05688c2b3e6c1f
+				  #x1f83d9abfb41bd6b
+				  #x5be0cd19137e2179)))
 
 (defvar s_sha_512_k
-  (setf s_sha_512_k (make-array 80
-				:element-type '(unsigned-byte 64)
-				:initial-contents '(#x428a2f98d728ae22
-						    #x7137449123ef65cd
-						    #xb5c0fbcfec4d3b2f
-						    #xe9b5dba58189dbbc
-						    #x3956c25bf348b538
-						    #x59f111f1b605d019
-						    #x923f82a4af194f9b
-						    #xab1c5ed5da6d8118
-						    #xd807aa98a3030242
-						    #x12835b0145706fbe
-						    #x243185be4ee4b28c
-						    #x550c7dc3d5ffb4e2
-						    #x72be5d74f27b896f
-						    #x80deb1fe3b1696b1
-						    #x9bdc06a725c71235
-						    #xc19bf174cf692694
-						    #xe49b69c19ef14ad2
-						    #xefbe4786384f25e3
-						    #x0fc19dc68b8cd5b5
-						    #x240ca1cc77ac9c65
-						    #x2de92c6f592b0275
-						    #x4a7484aa6ea6e483
-						    #x5cb0a9dcbd41fbd4
-						    #x76f988da831153b5
-						    #x983e5152ee66dfab
-						    #xa831c66d2db43210
-						    #xb00327c898fb213f
-						    #xbf597fc7beef0ee4
-						    #xc6e00bf33da88fc2
-						    #xd5a79147930aa725
-						    #x06ca6351e003826f
-						    #x142929670a0e6e70
-						    #x27b70a8546d22ffc
-						    #x2e1b21385c26c926
-						    #x4d2c6dfc5ac42aed
-						    #x53380d139d95b3df
-						    #x650a73548baf63de
-						    #x766a0abb3c77b2a8
-						    #x81c2c92e47edaee6
-						    #x92722c851482353b
-						    #xa2bfe8a14cf10364
-						    #xa81a664bbc423001
-						    #xc24b8b70d0f89791
-						    #xc76c51a30654be30
-						    #xd192e819d6ef5218
-						    #xd69906245565a910
-						    #xf40e35855771202a
-						    #x106aa07032bbd1b8
-						    #x19a4c116b8d2d0c8
-						    #x1e376c085141ab53
-						    #x2748774cdf8eeb99
-						    #x34b0bcb5e19b48a8
-						    #x391c0cb3c5c95a63
-						    #x4ed8aa4ae3418acb
-						    #x5b9cca4f7763e373
-						    #x682e6ff3d6b2b8a3
-						    #x748f82ee5defb2fc
-						    #x78a5636f43172f60
-						    #x84c87814a1f0ab72
-						    #x8cc702081a6439ec
-						    #x90befffa23631e28
-						    #xa4506cebde82bde9
-						    #xbef9a3f7b2c67915
-						    #xc67178f2e372532b
-						    #xca273eceea26619c
-						    #xd186b8c721c0c207
-						    #xeada7dd6cde0eb1e
-						    #xf57d4f7fee6ed178
-						    #x06f067aa72176fba
-						    #x0a637dc5a2c898a6
-						    #x113f9804bef90dae
-						    #x1b710b35131c471b
-						    #x28db77f523047d84
-						    #x32caab7b40c72493
-						    #x3c9ebe0a15c9bebc
-						    #x431d67c49c100d4c
-						    #x4cc5d4becb3e42b6
-						    #x597f299cfc657e2a
-						    #x5fcb6fab3ad6faec
-						    #x6c44198c4a475817))))
+  (make-array 80
+	      :element-type '(unsigned-byte 64)
+	      :initial-contents '(#x428a2f98d728ae22
+				  #x7137449123ef65cd
+				  #xb5c0fbcfec4d3b2f
+				  #xe9b5dba58189dbbc
+				  #x3956c25bf348b538
+				  #x59f111f1b605d019
+				  #x923f82a4af194f9b
+				  #xab1c5ed5da6d8118
+				  #xd807aa98a3030242
+				  #x12835b0145706fbe
+				  #x243185be4ee4b28c
+				  #x550c7dc3d5ffb4e2
+				  #x72be5d74f27b896f
+				  #x80deb1fe3b1696b1
+				  #x9bdc06a725c71235
+				  #xc19bf174cf692694
+				  #xe49b69c19ef14ad2
+				  #xefbe4786384f25e3
+				  #x0fc19dc68b8cd5b5
+				  #x240ca1cc77ac9c65
+				  #x2de92c6f592b0275
+				  #x4a7484aa6ea6e483
+				  #x5cb0a9dcbd41fbd4
+				  #x76f988da831153b5
+				  #x983e5152ee66dfab
+				  #xa831c66d2db43210
+				  #xb00327c898fb213f
+				  #xbf597fc7beef0ee4
+				  #xc6e00bf33da88fc2
+				  #xd5a79147930aa725
+				  #x06ca6351e003826f
+				  #x142929670a0e6e70
+				  #x27b70a8546d22ffc
+				  #x2e1b21385c26c926
+				  #x4d2c6dfc5ac42aed
+				  #x53380d139d95b3df
+				  #x650a73548baf63de
+				  #x766a0abb3c77b2a8
+				  #x81c2c92e47edaee6
+				  #x92722c851482353b
+				  #xa2bfe8a14cf10364
+				  #xa81a664bbc423001
+				  #xc24b8b70d0f89791
+				  #xc76c51a30654be30
+				  #xd192e819d6ef5218
+				  #xd69906245565a910
+				  #xf40e35855771202a
+				  #x106aa07032bbd1b8
+				  #x19a4c116b8d2d0c8
+				  #x1e376c085141ab53
+				  #x2748774cdf8eeb99
+				  #x34b0bcb5e19b48a8
+				  #x391c0cb3c5c95a63
+				  #x4ed8aa4ae3418acb
+				  #x5b9cca4f7763e373
+				  #x682e6ff3d6b2b8a3
+				  #x748f82ee5defb2fc
+				  #x78a5636f43172f60
+				  #x84c87814a1f0ab72
+				  #x8cc702081a6439ec
+				  #x90befffa23631e28
+				  #xa4506cebde82bde9
+				  #xbef9a3f7b2c67915
+				  #xc67178f2e372532b
+				  #xca273eceea26619c
+				  #xd186b8c721c0c207
+				  #xeada7dd6cde0eb1e
+				  #xf57d4f7fee6ed178
+				  #x06f067aa72176fba
+				  #x0a637dc5a2c898a6
+				  #x113f9804bef90dae
+				  #x1b710b35131c471b
+				  #x28db77f523047d84
+				  #x32caab7b40c72493
+				  #x3c9ebe0a15c9bebc
+				  #x431d67c49c100d4c
+				  #x4cc5d4becb3e42b6
+				  #x597f299cfc657e2a
+				  #x5fcb6fab3ad6faec
+				  #x6c44198c4a475817)))
 
 (defun bytes_to_number (data start)
   (let ((number 0))
