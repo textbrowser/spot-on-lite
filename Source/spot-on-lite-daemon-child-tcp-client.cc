7f095d0ca68d0bed566c79e4a13b51fa6054374a
Monster <textbrowser@gmail.com>
2018-07-25 12:11:22 -0400

    Empty content?

--- Source/spot-on-lite-daemon-child-tcp-client.cc
+++ Source/spot-on-lite-daemon-child-tcp-client.cc
@@ -928,7 +928,7 @@ void spot_on_lite_daemon_child_tcp_client::process_data(void)
 	}
   }
 
-  if(m_end_of_message_marker.isEmpty())
+  if(local_content.isEmpty() || m_end_of_message_marker.isEmpty())
     return;
 
   QList<QByteArray> list;
