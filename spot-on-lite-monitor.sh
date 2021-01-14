5ba7c1d35513a22ccd9fb8fd128fc23d5a7b5c31
textbrowser@gmail.com <textbrowser@gmail.com>
2021-01-14 10:55:16 -0500

    New file.

---
+++ spot-on-lite-monitor.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export AA_ENABLEHIGHDPISCALING=1
+export AA_USEHIGHDPIPIXMAPS=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
+# Disable https://en.wikipedia.org/wiki/MIT-SHM.
+
+export QT_X11_NO_MITSHM=1
+
+if [ -r ./Spot-On-Lite-Monitor ] && [ -x ./Spot-On-Lite-Monitor ]
+then
+    exec ./Spot-On-Lite-Monitor "$@"
+    exit $?
+elif [ -r /usr/local/spot-on-lite/Spot-On-Lite-Monitor ] && \
+     [ -x /usr/local/spot-on-lite/Spot-On-Lite-Monitor ]
+then
+    cd /usr/local/spot-on-lite/Spot-On-Lite-Monitor && \
+    exec ./Spot-On-Lite-Monitor "$@"
+    exit $?
+else
+    exit 1
+fi
