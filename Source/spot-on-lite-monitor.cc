311e63bebfd619ddded6008c189d661f679e73c7
textbrowser@gmail.com <textbrowser@gmail.com>
2021-01-15 15:00:45 -0500

    Statistics.

--- Source/spot-on-lite-monitor.cc
+++ Source/spot-on-lite-monitor.cc
@@ -32,10 +32,37 @@
 
 #include "spot-on-lite-monitor.h"
 
+static spot_on_lite_monitor::Columns field_name_to_column
+(const QString &field_name)
+{
+  if(field_name == "arguments")
+    return spot_on_lite_monitor::ARGUMENTS;
+  else if(field_name == "bytes_accumulated")
+    return spot_on_lite_monitor::BYTES_ACCUMULATED;
+  else if(field_name == "bytes_read")
+    return spot_on_lite_monitor::BYTES_READ;
+  else if(field_name == "bytes_written")
+    return spot_on_lite_monitor::BYTES_WRITTEN;
+  else if(field_name == "ip_information")
+    return spot_on_lite_monitor::IP_INFORMATION;
+  else if(field_name == "memory")
+    return spot_on_lite_monitor::MEMORY;
+  else if(field_name == "name")
+    return spot_on_lite_monitor::NAME;
+  else if(field_name == "pid")
+    return spot_on_lite_monitor::PID;
+  else if(field_name == "type")
+    return spot_on_lite_monitor::TYPE;
+  else
+    return spot_on_lite_monitor::ZZZ;
+}
+
 int main(int argc, char *argv[])
 {
+  Q_UNUSED(field_name_to_column);
+
   QApplication qapplication(argc, argv);
-  int rc = EXIT_SUCCESS;
+  auto rc = EXIT_SUCCESS;
 
   try
     {
@@ -58,13 +85,14 @@ int main(int argc, char *argv[])
 
 spot_on_lite_monitor::spot_on_lite_monitor(void):QMainWindow()
 {
+  qRegisterMetaType<QMap<Columns, QString> > ("QMap<Columns, QString>");
   m_ui.setupUi(this);
   m_future = QtConcurrent::run
     (this, &spot_on_lite_monitor::read_statistics_database);
   connect(this,
-	  SIGNAL(new_text(const QString &)),
+	  SIGNAL(added(const QMap<Columns, QString> &)),
 	  this,
-	  SLOT(slot_new_text(const QString &)));
+	  SLOT(slot_added(const QMap<Columns, QString> &)));
 }
 
 spot_on_lite_monitor::~spot_on_lite_monitor()
@@ -75,15 +103,7 @@ spot_on_lite_monitor::~spot_on_lite_monitor()
 
 void spot_on_lite_monitor::read_statistics_database(void)
 {
-  const QString columns("PID\t"
-			"Name\t"
-			"Type\t"
-			"IP Information\t"
-			"Bytes Accumulated\t"
-			"Bytes Read\t"
-			"Bytes Written\t"
-			"Memory\t"
-			"Arguments\n\n");
+  QMap<qint64, QMap<Columns, QString> > processes;
   const QString db_connection_id("1");
   const QString db_path(QDir::tempPath() +
 			QDir::separator() +
@@ -94,7 +114,7 @@ void spot_on_lite_monitor::read_statistics_database(void)
       if(m_future.isCanceled())
 	break;
 
-      QThread::sleep(100);
+      QThread::msleep(100);
 
       {
 	auto db = QSqlDatabase::addDatabase("QSQLITE", db_connection_id);
@@ -104,13 +124,47 @@ void spot_on_lite_monitor::read_statistics_database(void)
 	if(db.open())
 	  {
 	    QSqlQuery query(db);
-	    QString str("");
 
 	    query.setForwardOnly(true);
 
-	    if(query.exec("SELECT * FROM statistics"))
+	    if(query.exec("SELECT arguments, "  // 0
+			  "bytes_accumulated, " // 1
+			  "bytes_read, "        // 2
+			  "bytes_written, "     // 3
+			  "ip_information, "    // 4
+			  "memory, "            // 5
+			  "name, "              // 6
+			  "pid, "               // 7
+			  "type "               // 8
+			  "FROM statistics"))
 	      while(query.next())
 		{
+		  QMap<Columns, QString> values;
+
+		  values[ARGUMENTS] = query.value(0).toString();
+		  values[BYTES_ACCUMULATED] = query.value(1).toString();
+		  values[BYTES_READ] = query.value(2).toString();
+		  values[BYTES_WRITTEN] = query.value(3).toString();
+		  values[IP_INFORMATION] = query.value(4).toString();
+		  values[MEMORY] = query.value(5).toString();
+		  values[NAME] = query.value(6).toString();
+		  values[PID] = query.value(7).toString();
+		  values[TYPE] = query.value(8).toString();
+
+		  auto pid = values.value(PID).toLongLong();
+
+		  if(!processes.contains(pid))
+		    {
+		      processes[pid] = values;
+		      emit added(values);
+		    }
+		  else
+		    {
+		      if(processes.value(pid) != values)
+			emit changed(values);
+
+		      processes[pid] = values;
+		    }
 		}
 	  }
 
@@ -121,7 +175,46 @@ void spot_on_lite_monitor::read_statistics_database(void)
     }
 }
 
-void spot_on_lite_monitor::slot_new_text(const QString &text)
+void spot_on_lite_monitor::slot_added(const QMap<Columns, QString> &values)
 {
-  m_ui.text->setPlainText(text);
+  m_ui.processes->setSortingEnabled(false);
+  m_ui.processes->setRowCount(m_ui.processes->rowCount() + 1);
+  m_pid_to_row[values.value(PID).toLongLong()] =
+    m_ui.processes->rowCount() - 1;
+
+  QMapIterator<Columns, QString> it(values);
+  auto row = m_ui.processes->rowCount() - 1;
+  int i = 0;
+
+  while(it.hasNext())
+    {
+      it.next();
+
+      auto item = new QTableWidgetItem(it.value());
+
+      item->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+      m_ui.processes->setItem(row, i, item);
+      i += 1;
+    }
+
+  m_ui.processes->setSortingEnabled(true);
+}
+
+void spot_on_lite_monitor::slot_changed(const QMap<Columns, QString> &values)
+{
+  auto row = m_pid_to_row.value(values.value(PID).toLongLong(), -1);
+
+  if(row < 0)
+    return;
+
+  m_ui.processes->setSortingEnabled(false);
+  m_ui.processes->item(row, BYTES_ACCUMULATED)->setText
+    (values.value(BYTES_ACCUMULATED));
+  m_ui.processes->item(row, BYTES_READ)->setText(values.value(BYTES_READ));
+  m_ui.processes->item(row, BYTES_WRITTEN)->setText
+  (values.value(BYTES_WRITTEN));
+  m_ui.processes->item(row, IP_INFORMATION)->setText
+    (values.value(IP_INFORMATION));
+  m_ui.processes->item(row, MEMORY)->setText(values.value(MEMORY));
+  m_ui.processes->setSortingEnabled(true);
 }
