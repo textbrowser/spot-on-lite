e278ceb69833669b098b191825ae0a5f5c66da4c
textbrowser@gmail.com <textbrowser@gmail.com>
2022-03-30 20:52:43 -0400

    Debian release.

--- Distributions/build_deb.bash
+++ Distributions/build_deb.bash
@@ -5,6 +5,17 @@ make -j $(nproc)
 cp -p ./Icons/monitor.png ./usr/local/spot-on-lite/.
 cp -p ./Scripts/spot-on-lite-daemon.sh ./usr/local/spot-on-lite/.
 cp -p ./Spot-On-Lite* ./usr/local/spot-on-lite/.
+cp -p ./TO-DO ./usr/local/spot-on-lite/Documentation/.
 cp -p ./spot-on-lite-daemon.conf ./usr/local/spot-on-lite/.
 cp -p ./spot-on-lite-monitor.sh ./usr/local/spot-on-lite/.
 cp -pr ./Documentation/* ./usr/local/spot-on-lite/Documentation/.
+
+mkdir -p spot-on-lite-debian/usr/local
+mkdir -p spot-on-lite-debian/usr/share/applications
+cp -p ./spot-on-lite.desktop spot-on-lite-debian/usr/share/applications/.
+cp -pr ./DEBIAN spot-on-lite-debian/.
+cp -r ./usr/local/spot-on-lite spot-on-lite-debian/usr/local/.
+fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2022.03.30_amd64.deb
+rm -fr ./usr
+rm -fr spot-on-lite-debian
+make distclean
