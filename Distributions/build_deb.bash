2a3391ffdc2891e11da3a53fafd519a99450450a
textbrowser <textbrowser@gmail.com>
2022-07-05 11:39:03 -0400

    New version.

--- Distributions/build_deb.bash
+++ Distributions/build_deb.bash
@@ -13,8 +13,8 @@ cp -pr ./Documentation/* ./usr/local/spot-on-lite/Documentation/.
 mkdir -p spot-on-lite-debian/usr/local
 cp -pr ./DEBIAN spot-on-lite-debian/.
 cp -r ./usr/local/spot-on-lite spot-on-lite-debian/usr/local/.
-fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2022.05.25_amd64.deb
-mv Spot-On-Lite-2022.05.25_amd64.deb ~/Desktop/.
+fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2022.07.05_amd64.deb
+mv Spot-On-Lite-2022.07.05_amd64.deb ~/Desktop/.
 rm -fr ./usr
 rm -fr spot-on-lite-debian
 make distclean
