632c32c1eec479cc41f9e0f30b07950cef633b3e
textbrowser <textbrowser@gmail.com>
2022-07-05 21:03:07 -0400

    Pi.

--- Distributions/build_deb_armhf.bash
+++ Distributions/build_deb_armhf.bash
@@ -11,7 +11,7 @@ cp -p ./spot-on-lite-monitor.sh ./usr/local/spot-on-lite/.
 cp -pr ./Documentation/* ./usr/local/spot-on-lite/Documentation/.
 
 mkdir -p spot-on-lite-debian/usr/local
-cp -pr ./RASPBERRY spot-on-lite-debian/DEBIAN.
+cp -pr ./RASPBERRY spot-on-lite-debian/DEBIAN
 cp -r ./usr/local/spot-on-lite spot-on-lite-debian/usr/local/.
 fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2022.07.05_armhf.deb
 mv Spot-On-Lite-2022.07.05_armhf.deb ~/Desktop/.
