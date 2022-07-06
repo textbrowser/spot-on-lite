3bdb3c691b99245b2481d9c65f0ffcda99aec694
textbrowser <textbrowser@gmail.com>
2022-07-05 20:54:14 -0400

    Pi.

---
+++ Distributions/build_deb_armhf.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+mkdir -p ./usr/local/spot-on-lite/Documentation
+make -j $(nproc)
+cp -p ./Icons/monitor.png ./usr/local/spot-on-lite/.
+cp -p ./Scripts/spot-on-lite-daemon.sh ./usr/local/spot-on-lite/.
+cp -p ./Spot-On-Lite* ./usr/local/spot-on-lite/.
+cp -p ./TO-DO ./usr/local/spot-on-lite/Documentation/.
+cp -p ./spot-on-lite-daemon.conf ./usr/local/spot-on-lite/.
+cp -p ./spot-on-lite-monitor.sh ./usr/local/spot-on-lite/.
+cp -pr ./Documentation/* ./usr/local/spot-on-lite/Documentation/.
+
+mkdir -p spot-on-lite-raspberry/usr/local
+cp -pr ./RASPBERRY spot-on-lite-raspberry/.
+cp -r ./usr/local/spot-on-lite spot-on-lite-raspberry/usr/local/.
+fakeroot dpkg-deb --build spot-on-lite-raspberry Spot-On-Lite-2022.07.05_armhf.deb
+mv Spot-On-Lite-2022.07.05_armhf.deb ~/Desktop/.
+rm -fr ./usr
+rm -fr spot-on-lite-raspberry
+make distclean
