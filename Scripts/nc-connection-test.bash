d7c24a7efc137379d5648b9f262151284992a88c
textbrowser <textbrowser@gmail.com>
2022-09-13 07:53:50 -0400

    Discover the interpreter.

--- Scripts/nc-connection-test.bash
+++ Scripts/nc-connection-test.bash
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # A test script.
 
 for i in {1..100};
 do
-    nc -d tulip-ipv4.tilaa.cloud 4710 1>/dev/null &
+    nc -d 127.0.0.1 4710 1>/dev/null &
 done
--- Scripts/openssl-connection-test.bash
+++ Scripts/openssl-connection-test.bash
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # A test script.
 
 for i in {1..256};
 do
-    openssl s_client -connect tulip-ipv4.tilaa.cloud:4710 &
+    openssl s_client -connect 127.0.0.1:4710 &
 done
 
 sleep 10
