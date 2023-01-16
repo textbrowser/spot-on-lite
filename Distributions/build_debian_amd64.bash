dffe007a504e69e733d94d76cb89a2c24e277c61
textbrowser <textbrowser@gmail.com>
2023-01-15 20:15:19 -0500

    New release.

--- Distributions/build_debian_amd64.bash
+++ Distributions/build_debian_amd64.bash
@@ -25,8 +25,8 @@ cp -pr ./Documentation/* ./usr/local/spot-on-lite/Documentation/.
 mkdir -p spot-on-lite-debian/usr/local
 cp -pr ./DEBIAN spot-on-lite-debian/.
 cp -r ./usr/local/spot-on-lite spot-on-lite-debian/usr/local/.
-fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2022.12.10_amd64.deb
-mv Spot-On-Lite-2022.12.10_amd64.deb ~/Desktop/.
+fakeroot dpkg-deb --build spot-on-lite-debian Spot-On-Lite-2023.01.15_amd64.deb
+mv Spot-On-Lite-2023.01.15_amd64.deb ~/Desktop/.
 rm -fr ./usr
 rm -fr spot-on-lite-debian
 make distclean
--- Source/spot-on-lite-daemon-main.cc
+++ Source/spot-on-lite-daemon-main.cc
@@ -236,7 +236,7 @@ static int prepare_signal_handlers(void)
   return 0;
 }
 
-static std::string s_version = "2022.12.10";
+static std::string s_version = "2023.01.15";
 
 int main(int argc, char *argv[])
 {
