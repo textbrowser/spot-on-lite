064440714eebc1bc900b25437e251cdc025831e1
textbrowser <textbrowser@gmail.com>
2024-09-22 22:29:06 -0400

    Format.

--- Scripts/spot-on-lite-daemon.sh
+++ Scripts/spot-on-lite-daemon.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 # Alexis Megas.
 
-cd /usr/local/spot-on-lite && exec ./Spot-On-Lite-Daemon --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf
+cd /usr/local/spot-on-lite && \
+exec ./Spot-On-Lite-Daemon \
+     --configuration-file /usr/local/spot-on-lite/spot-on-lite-daemon.conf
 exit $?
