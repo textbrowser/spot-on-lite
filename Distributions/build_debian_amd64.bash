4c91b8958ea2144c8853d45f3f15bc4059323710
textbrowser <textbrowser@gmail.com>
2025-02-22 16:45:31 -0500

    Distributions.

--- Distributions/build_debian_amd64.bash
+++ Distributions/build_debian_amd64.bash
@@ -23,13 +23,13 @@ fi
 make distclean 2>/dev/null
 mkdir -p ./opt/spot-on-lite/Documentation
 make -j $(nproc)
-cp -p ./Documentation/REMINDERS ./opt/spot-on-lite/Documentation/.
 cp -p ./Icons/monitor.png ./opt/spot-on-lite/.
 cp -p ./Scripts/spot-on-lite-daemon.sh ./opt/spot-on-lite/.
 cp -p ./Spot-On-Lite* ./opt/spot-on-lite/.
 cp -p ./spot-on-lite-daemon.conf ./opt/spot-on-lite/.
 cp -p ./spot-on-lite-monitor.sh ./opt/spot-on-lite/.
 cp -pr ./Documentation/* ./opt/spot-on-lite/Documentation/.
+rm -fr ./opt/spot-on-lite-debian/Documentation/*.qrc
 mkdir -p spot-on-lite-debian/opt
 cp -pr ./Distributions/DEBIAN spot-on-lite-debian/.
 cp -r ./opt/spot-on-lite spot-on-lite-debian/opt/.
--- Distributions/build_debian_arm.bash
+++ Distributions/build_debian_arm.bash
@@ -29,13 +29,13 @@ fi
 make distclean 2>/dev/null
 mkdir -p ./opt/spot-on-lite/Documentation
 make -j $(nproc)
-cp -p ./Documentation/REMINDERS ./opt/spot-on-lite/Documentation/.
 cp -p ./Icons/monitor.png ./opt/spot-on-lite/.
 cp -p ./Scripts/spot-on-lite-daemon.sh ./opt/spot-on-lite/.
 cp -p ./Spot-On-Lite* ./opt/spot-on-lite/.
 cp -p ./spot-on-lite-daemon.conf ./opt/spot-on-lite/.
 cp -p ./spot-on-lite-monitor.sh ./opt/spot-on-lite/.
 cp -pr ./Documentation/* ./opt/spot-on-lite/Documentation/.
+rm -fr ./opt/spot-on-lite-debian/Documentation/*.qrc
 mkdir -p spot-on-lite-debian/opt
 
 architecture="$(dpkg --print-architecture)"
