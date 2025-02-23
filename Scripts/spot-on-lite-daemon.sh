cd3da50f4a2939c6487c3854852f439ca185d16c
textbrowser <textbrowser@gmail.com>
2025-02-23 11:46:00 -0500

    Report status.

--- Scripts/spot-on-lite-daemon.sh
+++ Scripts/spot-on-lite-daemon.sh
@@ -2,7 +2,24 @@
 
 # Alexis Megas.
 
-cd /opt/spot-on-lite && \
-exec ./Spot-On-Lite-Daemon \
-     --configuration-file /opt/spot-on-lite/spot-on-lite-daemon.conf
-exit $?
+if [ -r /opt/spot-on-lite ] && [ -x /opt/spot-on-lite ]
+then
+    cd /opt/spot-on-lite && \
+    ./Spot-On-Lite-Daemon \
+    --configuration-file /opt/spot-on-lite/spot-on-lite-daemon.conf
+
+    rc=$?
+
+    if [ $rc -eq 0 ]
+    then
+	echo "Spot-On-Lite-Daemon was launched."
+    else
+	echo "Could not launch Spot-On-Lite-Daemon."
+	exit $rc
+    fi
+else
+    echo "The directory /opt/spot-on-lite is not valid."
+    exit 1
+fi
+
+exit 0
